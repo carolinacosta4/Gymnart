6ca62589f19e4a4c37c359c63045f05665746b10
Carolina Costa <carolinamccosta@gmail.com>
2023-11-30 16:48:54 +0000

    initial commit

---
+++ api/api.js
@@ -0,0 +1,59 @@
+import fetchMock from "fetch-mock";
+import athlete from './mocks/mocks'
+
+fetchMock.mock('', athlete)
+
+
+/**
+ * GET utility function
+ * @param {string} apiBaseUrl - this is the base URL from the API
+ * @param {string} endpoint - this is the endpoint of the API
+ * @returns a promise object with the response
+ */
+
+export async function get(apiBaseUrl, endpoint){
+    try {
+        const response = await fetch(`${apiBaseUrl}/${endpoint}`)
+        return handleResponse(response)
+    } catch (error) {
+        throw Error
+    }
+}
+
+/**
+ * POST utility function
+ * @param {*} apiBaseUrl - this is the base URL from the API
+ * @param {*} endpoint - this is the endpoint of the API
+ * @param {*} data - this is the data that we will send to the server
+ */
+
+export async function post(apiBaseUrl, endpoint, data){
+    try {
+        const response = await fetch(`${apiBaseUrl}/${endpoint}`,
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body:JSON.stringify(data)
+        })
+        return handleResponse(response)
+    } catch (error) {
+        throw error
+    }
+}
+
+/**
+ * Response handling 
+ * @param {object} response a promise object with the response of the server
+ * @returns the response from the server 
+ */
+
+async function handleResponse(response){
+    if(!response.ok){
+        const errorMessage = await response.text()
+        throw new Error(`API request failed with status ${response.status}: ${errorMessage}`)
+    }
+    const data = await response.json()
+    return data
+}
---
+++ src/main.js
@@ -0,0 +1,28 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+app.use(vuetify)
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
