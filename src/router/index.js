52b5b73267e5e055f28b0961e72fe394607da39c
Carolina Costa <carolinamccosta@gmail.com>
2023-12-28 16:30:40 +0000

    small change

--- src/router/index.js
+++ src/router/index.js
@@ -37,7 +37,7 @@ const router = createRouter({
       component: DashboardView
     },
     {
-      path: '/:id',
+      path: '/account/:id',
       name: 'account',
       component: UserPageView
     },
