ce25afd5864be49b4773177689fb3ff448f266e9
Beatriz Lisboa <btrzlisboa037@gmail.com>
2023-12-30 19:59:01 +0000

    nav bar lateral hover effect + correcoes na lp

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,20 +1,43 @@
 <template>
     <div id="header">
-        <RouterLink :to="{ name: 'account', params:{id: user.username}  }" class="text"><img src="../assets/iconsNav/user.svg" alt="user icon" class="icon"></RouterLink>
+        <RouterLink :to="{ name: 'account', params:{id: user.username}  }" class="text"><img src="../assets/iconsNav/user.svg" alt="user icon" class="icon" id="user" @click="addToList('user')"></RouterLink>
         <span class="pr-3"></span>
         <div id="mainIcons">
-            <RouterLink :to="{ name: 'search' }" class="text" ><img src="../assets/iconsNav/search.svg" alt="console icon outlined" id="search" class="icon" @click="addToList('search')"></RouterLink>
-            <span class="pr-3"></span>
-            <RouterLink :to="{ name: 'home' }" class="text"><img src="../assets/iconsNav/home.svg" alt="home icon outlined" id="home" class="icon" @click="addToList('home')"></RouterLink>
-            <span class="pr-3"></span>
-            <RouterLink :to="{name: 'dashboard'}" class="text"><img src="../assets/iconsNav/dashboard.svg" alt="dashboard icon outlined" id="dashboard" class="icon" @click="addToList('dashboard')"></RouterLink>
-            <span class="pr-3"></span>
-            <RouterLink :to="{ name: 'favorites' }" class="text"><img src="../assets/iconsNav/favorites.svg" alt="heart icon outlined" id="favorites" class="icon" @click="addToList('favorites')"></RouterLink>
-            <span class="pr-3"></span>
-            <RouterLink :to="{ name: 'calendar' }" class="text"><img src="../assets/iconsNav/calendar.svg" alt="calendar icon outlined" id="calendar" class="icon" @click="addToList('calendar')"></RouterLink>
-            <span class="pr-3"></span>
-            <RouterLink :to="{ name: 'quiz' }" class="text"><img src="../assets/iconsNav/quiz.svg" alt="console icon outlined" class="icon" id="quiz" @click="addToList('quiz')"></RouterLink>
-            <span class="pr-3"></span>
+            <div class="divHover">
+                <RouterLink :to="{ name: 'search' }" class="text" ><img src="../assets/iconsNav/search.svg" alt="console icon outlined" id="search" class="icon" @click="addToList('search')"></RouterLink>
+                <img src="../assets/iconsNav/searchHover.svg" class="hover" alt="" id="searchHover">
+                <span class="pr-3"></span>
+            </div>
+
+            <div class="divHover">
+                <RouterLink :to="{ name: 'home' }" class="text"><img src="../assets/iconsNav/home.svg" alt="home icon outlined" id="home" class="icon" @click="addToList('home')"></RouterLink>
+                <img src="../assets/iconsNav/homeHover.svg" class="hover" alt="" id="homeHover">
+                <span class="pr-3"></span>
+            </div>
+
+            <div class="divHover">
+                <RouterLink :to="{name: 'dashboard'}" class="text"><img src="../assets/iconsNav/dashboard.svg" alt="dashboard icon outlined" id="dashboard" class="icon" @click="addToList('dashboard')"></RouterLink>
+                <img src="../assets/iconsNav/dashboardHover.svg" class="hover" alt="" id="dashboardHover">
+                <span class="pr-3"></span>
+            </div>
+
+            <div class="divHover">
+                <RouterLink :to="{ name: 'favorites' }" class="text"><img src="../assets/iconsNav/favorites.svg" alt="heart icon outlined" id="favorites" class="icon" @click="addToList('favorites')"></RouterLink>
+                <img src="../assets/iconsNav/favoritesHover.svg" class="hover" alt="" id="favoritesHover">
+                <span class="pr-3"></span>
+            </div>
+
+            <div class="divHover">
+                <RouterLink :to="{ name: 'calendar' }" class="text"><img src="../assets/iconsNav/calendar.svg" alt="calendar icon outlined" id="calendar" class="icon" @click="addToList('calendar')"></RouterLink>
+                <img src="../assets/iconsNav/calendarHover.svg" class="hover" alt="" id="calendarHover">
+                <span class="pr-3"></span>
+            </div>
+
+            <div class="divHover">
+                <RouterLink :to="{ name: 'quiz' }" class="text"><img src="../assets/iconsNav/quiz.svg" alt="console icon outlined" class="icon" id="quiz" @click="addToList('quiz')"></RouterLink>
+                <img src="../assets/iconsNav/quizHover.svg" class="hover" alt="" id="quizHover">
+                <span class="pr-3"></span>
+            </div>
         </div>
         <RouterLink :to="{ name: 'landingPage' }" class="text" @click="logout()"><img src="../assets/iconsNav/logout.svg" alt="logout icon outlined" class="icon"></RouterLink>
     </div>
@@ -47,7 +70,12 @@ export default {
             this.store.logout();
         },
         addToList(idIcon){
-            if (clickedIcon.length > 0){
+            if (idIcon === 'user'){
+                let previousId = clickedIcon[0]
+                this.changeIcon(previousId, 'blank')
+                clickedIcon = []
+            }
+            else if (clickedIcon.length > 0 && idIcon !== 'user'){
                 let previousId = clickedIcon[0]
                 this.changeIcon(previousId, 'blank')
                 
@@ -55,9 +83,9 @@ export default {
                 clickedIcon.push(idIcon)
                 this.changeIcon(idIcon, 'filled')
             }
-            else {
+            else if (clickedIcon = []){
+                this.changeIcon(idIcon, 'filled')
                 clickedIcon.push(idIcon)
-                this.changeIcon(idIcon, 'blank')
             }
             
         },
@@ -101,7 +129,6 @@ let clickedIcon = []
 .icon{
     height: auto;
     width: 2em;
-    margin-bottom: 1em;
 }
 
 .icon:hover{
@@ -118,5 +145,29 @@ let clickedIcon = []
     justify-content: center;
     align-items: center;
     margin-top: -6em;
+    margin-left: 1em;
+    row-gap: 1em;
+}
+
+.divHover{
+    position: relative;
+    display: inline-block;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+}
+
+.hover {
+  display: none;
+  position: absolute;
+  top: 0;
+  left: 0;
+  margin-left: 2.5em;
+  width: 9em;
+}
+
+.divHover:hover .hover {
+  display: block;
 }
 </style>
