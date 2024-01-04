88d615d946d84ff87a7b8ccc5db7df762f8e53b4
VascoLima21 <you@example.com>
2024-01-04 16:42:39 +0000

    Athletes More Updates

--- src/views/AthleteView.vue
+++ src/views/AthleteView.vue
@@ -1,5 +1,5 @@
 <template>
-    <div style="margin-left: 200px">
+    <div id="parentContainer">
         <div id="headerInfo">
             <h1 class="title">{{ athleteStore.getAthlete(id).name }}</h1>
             <img :src="teamStore.getTeam(athleteStore.getAthlete(id).teamAcronym).flagIcon" alt="">
@@ -9,45 +9,46 @@
         <div id="athleteInfo">
             <div class="smallInfo">
                 <img src="../assets/iconsAthlete/ageIcon.svg" alt="">
-                <p style="color: purple">{{ athleteStore.getAthlete(id).age }} years</p>
+                <p>{{ athleteStore.getAthlete(id).age }} years</p>
             </div>
             <div class="smallInfo">
                 <img src="../assets/iconsAthlete/birthPlaceIcon.svg" alt="">
-                <!-- <p style="color: purple">{{ athleteStore.getAthlete(id).birthPlace }}</p> -->
+                <!-- <p>{{ athleteStore.getAthlete(id).birthPlace }}</p> -->
             </div>
             <div class="smallInfo">
                 <img src="../assets/iconsAthlete/heightIcon.svg" alt="">
-                <!-- <p style="color: purple">{{ athleteStore.getAthlete(id).height }}({{ athleteStore.getAthlete(id).height * 3.2808 }}ft)</p> -->
+                <!-- <p>{{ athleteStore.getAthlete(id).height }}({{ athleteStore.getAthlete(id).height * 3.2808 }}ft)</p> -->
             </div>
             <div class="smallInfo">
                 <img src="../assets/iconsAthlete/firstOlympicsIcon.svg" alt="">
-                <!-- <p style="color: purple">{{ athleteStore.getAthlete(id).firstOlympics}}</p> -->
+                <!-- <p>{{ athleteStore.getAthlete(id).firstOlympics}}</p> -->
             </div>
         </div>
-        <h1 style="color: blue">Olympic Medals</h1>
-
+        
+        <h1 class="title">Olympic Medals</h1>
         <div id="medals">
-            <div id="silverMedals">
+            <div class="medalsTextAlign" id="silverMedals">
                 <img src="../assets/iconsAthlete/silverMedal.svg" alt="">
-                <p style="color: silver">{{ athleteStore.getAthlete(id).silver }}</p>
+                <p class="medalsNumber">{{ athleteStore.getAthlete(id).silver }}</p>
 
             </div>
-            <div id="goldMedals">
+            <div class="medalsTextAlign" id="goldMedals">
                 <img src="../assets/iconsAthlete/goldMedal.svg" alt="">
-                <p style="color: gold">{{ athleteStore.getAthlete(id).gold }}</p>
+                <p class="medalsNumber">{{ athleteStore.getAthlete(id).gold }}</p>
             </div>
-            <div id="bronzeMedals">
+            <div class="medalsTextAlign" id="bronzeMedals">
                 <img src="../assets/iconsAthlete/bronzeMedal.svg" alt="">
-                <p style="color: brown">{{ athleteStore.getAthlete(id).bronze }}</p>
+                <p class="medalsNumber">{{ athleteStore.getAthlete(id).bronze }}</p>
 
             </div>
             <div id="lastResult">
-                <p style="color: brown">Last Result: {{ athleteStore.getAthlete(id).lastResult }}</p>
+                <p class="subTitle">Last Result:</p>
+                <p class="lastResultText">{{ athleteStore.getAthlete(id).lastResult }}</p>
             </div>
         </div>
         <div>
-            <h1 style="color: blue">Curiosities</h1>
-            <p style="color: pink">{{ athleteStore.getAthlete(id).curiosities }}</p>
+            <h1 class="title">Curiosities</h1>
+            <p class="text">{{ athleteStore.getAthlete(id).curiosities }}</p>
         </div>
         <div>
             <img :src="athleteStore.getAthlete(id).picturePath" alt="">
@@ -113,6 +114,10 @@ export default {
     src: url(@/assets/Lexend_Deca/LexendDeca-Bold.ttf);
 }
 
+#parentContainer{
+    padding: 2rem;
+    margin-left: 6em;
+}
 
 #headerInfo {
     display: flex;
@@ -122,6 +127,8 @@ export default {
 .smallInfo {
     display: flex;
     flex-direction: row;
+    color: black;
+    font-family: Lexend Deca ExtraLight;
 }
 
 #medals {
@@ -129,9 +136,35 @@ export default {
     flex-direction: row;
 }
 
+.medalsTextAlign{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
 .title {
     color: #ED2E2F;
     font-family: Saphile;
     font-size: 36px;
+}
+
+.subTitle{
+    color: #ED2E2F;
+    font-family: Lexend Deca Regular;
+}
+
+.lastResultText{
+    color: #F16A64;
+    font-family: Lexend Deca Light;
+}
+
+.medalsNumber{
+    color: #F16A64;
+    font-family: Lexend Deca Bold;
+}
+
+.text{
+    color: black;
+    font-family: Lexend Deca ExtraLight;
 }
 </style>
